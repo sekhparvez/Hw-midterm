1cdb6c2ac5f53e6abceaa1427d94c3e171c88732
sekhparvez <sekhparvez@gmail.com>
2021-10-08 22:23:42 -0400

    final midterm

---
+++ hw/js/data.js
@@ -0,0 +1,19 @@
+var data = [
+    {
+    section: 'reviews',
+    story: "He walked down the steps from the train station in a bit of a hurry knowing the secrets in the briefcase must be secured as quickly as possible. Bounding down the steps, he heard something behind him and quickly turned in a panic. There was nobody there but a pair of old worn-out shoes were placed neatly on the steps he had just come down. Had he past them without seeing them? It didn't seem possible. He was about to turn and be on his way when a deep chill filled his body."
+    },
+
+    {
+        section: 'recipes',
+        story: "Koreans,” according to Vice magazine, “are the undisputed Asian masters of chicken.” I suspect there may be some dispute over this, but even a few Filipinos, themselves no slouches when it comes to chicken, concede South Korea’s mastery of the deep-fat frier. Despite its ubiquity – in 2016, there were more than twice as many fried chicken restaurants in the country than all the hamburger, pizza and sandwich joints put together – KFC is a relatively recent phenomenon, introduced, it’s said, by US soldiers during the Korean War, and made more accessible by falling food prices and a rise in disposable income in the decades that followed. What was once an occasional treat has become the takeaway of choice, perfect for sharing with kids or to partner a cold beer. "
+    },
+
+    {
+        section: 'delivery',
+        story: "One of the reasons often cited for Korean fried chicken’s extraordinary crispness is the double frying technique, where it’s first fried at a relatively low temperature until the chicken is cooked through, then again at a higher temperature to set the crust. Joo and López-Alt don’t bother doing this, with the latter explaining that he doesn’t feel it makes much difference with such a thin batter, “other than allowing you to rapidly re-heat and serve large portions of par-cooked chicken”, which is probably why it’s popular in restaurants. Geary, however, argues that the lower temperature is vital for driving all the moisture out of the meat without overcooking it, thus making for a more crisp result. López-Alt’s chicken is deliciously juicy and crunchy straight out of the fryer, but the double-fried varieties do seem to stand up better to the sauce, so I’d recommend going the extra mile."
+    }
+    
+        
+]
+
--- hw/js/hw.js
+++ hw/js/hw.js
@@ -1,14 +1,63 @@
-var change = document.querySelectorAll("nav a");
-document.addEventListener("click", switchT);
 
-function switchT(event) {
-  event.preventDefault();
-  if (event.target.matches("nav a")) {
-    for (let item of change) {
-      item.classList.remove("active");
+// var change = document.querySelectorAll("nav a");
+// var content = document.querySelector(".content");
+// document.addEventListener("click", switchT);
+
+// function switchT(event) {
+  
+//   if (event.target.matches("nav a")) {
+  
+//     for (let item of change) {
+//       item.classList.remove("active");
+//     }
+//     event.target.classList.add("active");
+
+// }
+// }
+// variables
+var tabs = document.querySelectorAll('nav a')
+var contentPara = document.querySelector('.content')
+
+// when the hash changes
+function setActiveTabAccordingToHash(type) {
+  makeAllTabsInactive()
+  var tabToActivate = document.querySelector(`[href="#${type}"]`)
+  tabToActivate.classList.add('active')
+}
+
+function makeAllTabsInactive() {
+  tabs.forEach((tab) => tab.classList.remove('active'))
+}
+
+// runs on page load and whenever the hash changes
+function setContentAccordingToHash() {
+  const type = window.location.hash.substring(1)
+  for (var item of data) {
+    if (item.section === type) {
+      contentPara.innerHTML = `
+      <h2>${item.section}</h2> 
+      <p>${item.story}</p>`
+
+      setActiveTabAccordingToHash(type)
     }
-    event.target.classList.add("active");//question to why use event and not item?
   }
 }
 
+// only runs once on page load
+function initializePage() {
+  if (!window.location.hash) {
+    window.location.hash = 'cuisines'
+    document.querySelector('[href="#cuisines"]').classList.add('active')
+  } else {
+    document
+      .querySelector(`[href="${window.location.hash}"]`)
+      .classList.add('active')
+  }
+  setContentAccordingToHash()
+}
 
+// event listeners
+window.addEventListener('hashchange', setContentAccordingToHash)
+
+// initializePage()
+document.addEventListener('DOMContentLoaded', initializePage)
